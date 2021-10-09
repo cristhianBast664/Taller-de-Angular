428f7575b77de159f2d1951117b84718c452924e
Cristhian Bastidas <cristhian@udenar.edu.co>
2021-10-08 20:42:42 -0500

    Taller Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de personas';
+  nombreInput:string = '';
+  apellidoInput:string = '';
+
+  personas: Persona[] = [new Persona('Cristhian','Bastidas'),
+                         new Persona('Camilo', 'Benavides'),
+                         new Persona('Carlos', 'Perez')];
+
+  agregarPersona() {
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1);
+
+  }
+
 }
+
